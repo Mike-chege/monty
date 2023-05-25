d5f2a3ad486f91ae179d8d2ecbbf3aaec1e8de01
mikechege01 <mike@mail.com>
2023-05-25 16:38:30 -0700

    Ready for [push] insertion of elements

---
+++ insert_el.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * push_m - adds new node to the stack
+ * @head: pointer to the head of the stack
+ * @counter: line_number
+ * Return: void
+*/
+void push_m(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		new_node(head, n);
+	else
+		new_queue(head, n);
+}
+
