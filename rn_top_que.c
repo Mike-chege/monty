9cb88cb6bb75134eed77bb8a90b3aea2b9160190
mikechege01 <mike@mail.com>
2023-05-26 00:47:17 -0700

    Task[rn_top_que_stck.c] runs various convertions/fixed betty style

--- rn_top_que.c
+++ rn_top_que.c
@@ -48,3 +48,16 @@ void new_queue(stack_t **head, int n)
 	}
 }
 
+/**
+ * stack_conv - also displays the top most elelment
+ * @head: pointer to the head of the stack
+ * @counter: line_number
+ * Return: Always (void) Nothing
+*/
+void stack_conv(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
+
